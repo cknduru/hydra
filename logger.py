434e2f36cebce040c003a49baa548e06b532df00
Netix <netix@net.com>
2020-04-15 22:16:34 +0200

    Added timestamps to logging
    Signed-off-by: Netix <netix@net.com>

--- logger.py
+++ logger.py
@@ -1,24 +1,30 @@
+from time import localtime, strftime
+
+def get_time():
+	return str(strftime("%d-%m-%Y %H:%M:%S", localtime()))
+
 def write(module, msg):
 	flog = open('log.txt', 'a')
-	log_message = '[*] {}: {}'.format(module, msg)
+	log_message = '[*] {} {}: {}'.format(get_time(), module, msg)
 
 	flog.write(log_message + '\n')
 	flog.close()
 
 def write_e(module, msg):
 	flog = open('log.txt', 'a')
-	log_message = '[/] {}: {}'.format(module, msg)
+	log_message = '[/] {} {}: {}'.format(get_time(), module, msg)
 
 	flog.write(log_message + '\n')
 	flog.close()
 
 def write_m(module, sensor_num, reading):
 	flog = open('log.txt', 'a')
-	log_message = '[*] {}: sensor_num = {}, reading = {}'.format(module, sensor_num, reading)
+	log_message = '[*] {} {}: sensor_num = {}, reading = {}'.format(get_time(), module, sensor_num, reading)
 
 	flog.write(log_message + '\n')
 	flog.close()
 
 def setup():
+	# overwrite file on each start
 	flog = open('log.txt', 'w')
 	flog.write('')
--- temperature_sensor.py
+++ temperature_sensor.py
@@ -8,6 +8,8 @@ q_out = None
 
 # note that q_in / q_out are flipped compared to in main.py on purpose
 def setup(q_out, q_in):
-	time.sleep(4)
-	q_out.append('{} {} {}'.format(MODULE, 2, 25.0))
 	logger.write(MODULE, 'booted')
+	
+	while True:
+		time.sleep(4)
+		q_out.append('{} {} {}'.format(MODULE, 2, 25.0))
